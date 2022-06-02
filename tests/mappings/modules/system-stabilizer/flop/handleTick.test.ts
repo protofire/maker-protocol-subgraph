8dc11d2f5d7e9c49dc5210cedba7dddbcc5901db
Tomas Rawski <tomasrawski@carnesvalidadas.com>
2022-06-02 16:44:36 -0300

    flopper#handleTick mapping and test

--- tests/mappings/modules/system-stabilizer/flop/handleTick.test.ts
+++ tests/mappings/modules/system-stabilizer/flop/handleTick.test.ts
@@ -1,10 +1,22 @@
 import { Bytes, Address, BigInt, BigDecimal } from '@graphprotocol/graph-ts'
-import { units } from '@protofire/subgraph-toolkit'
-import { test, clearStore, assert } from 'matchstick-as'
+import { units, bytes } from '@protofire/subgraph-toolkit'
+import { test, clearStore, assert, log } from 'matchstick-as'
 import { LogNote } from '../../../../../generated/Flop/Flopper'
 import { handleTick } from '../../../../../src/mappings/modules/system-stabilizer/flop'
+import { mockDebt } from '../../../../helpers/mockedFunctions'
 import { tests } from '../../../../../src/mappings/modules/tests'
 import { Auctions, system as systemModule } from '../../../../../src/entities'
+
+function createEvent(id: BigInt): LogNote {
+  let sig = tests.helpers.params.getBytes('sig', Bytes.fromHexString('0xfc7b6aee'))
+  let arg1 = tests.helpers.params.getBytes('arg1', Bytes.fromUTF8(''))
+  let radBytes = Bytes.fromUint8Array(Bytes.fromBigInt(id).reverse())
+  let arg2 = tests.helpers.params.getBytes('arg2', radBytes)
+
+  let event = changetype<LogNote>(tests.helpers.events.getNewEvent([sig, arg1, arg2]))
+
+  return event
+}
 
 test('Flopper#handleTick updates Auction.quantity and Auction.endTime', () => {
   let bidId = BigInt.fromString('1')
@@ -12,25 +24,20 @@ test('Flopper#handleTick updates Auction.quantity and Auction.endTime', () => {
 
   let ONE = units.WAD
   let auctionBidDuration = BigInt.fromI32(172800) // 2 days | ttl
-  let lotSizeIncrease = BigDecimal.fromString('1500000000000000000') // 1.50E18 | pad
+  let lotSizeIncrease = BigDecimal.fromString('1.5') // 1.50E18 | pad
 
-  let quantity = BigInt.fromString('10000000000000000000000000000000000000000000000000') // lot
+  let quantity = BigInt.fromString('10') // lot
 
-  let signature = '0xfc7b6aee'
-  let sig = tests.helpers.params.getBytes('sig', Bytes.fromHexString(signature))
-  let arg1 = tests.helpers.params.getBytes('arg1', Bytes.fromUTF8(bidId.toString()))
+  let event = createEvent(bidId)
 
-  let event = changetype<LogNote>(tests.helpers.events.getNewEvent([sig, arg1]))
-
-  let UPDATED_ADDRESS = '0x4d95a049d5b0b7d32058cd3f2163015747522e99'
-  let address = Address.fromString(UPDATED_ADDRESS)
-  event.address = address
-
-  let auction = Auctions.loadOrCreateAuction(auctionId, event)
+  let auction = Auctions.loadOrCreateAuction(auctionId, event) // load the Auction with the quantity value
   auction.endTime = event.block.timestamp
   auction.quantity = quantity
+  auction.save()
 
-  let system = systemModule.getSystemState(event) // load the vars in the system entity
+  mockDebt()
+
+  let system = systemModule.getSystemState(event) // load the vars from the system entity
   system.debtAuctionBidDuration = auctionBidDuration
   system.debtAuctionLotSizeIncrease = lotSizeIncrease
   system.save()
@@ -39,11 +46,10 @@ test('Flopper#handleTick updates Auction.quantity and Auction.endTime', () => {
 
   // Make the calcs that are inside the handleTick function
   let endTime = event.block.timestamp.plus(auctionBidDuration)
-  let finalQuantity: BigInt = units.toRad(lotSizeIncrease.plus(quantity.toBigDecimal()).div(ONE))
 
   // Test
-  assert.fieldEquals('Auction', auctionId, 'endTime', endTime.toString())
-  assert.fieldEquals('Auction', auctionId, 'quantity', finalQuantity.toString())
+  assert.fieldEquals('Auction', auctionId, 'endTime', endTime.toString()) //'172801'
+  assert.fieldEquals('Auction', auctionId, 'quantity', '15')
 
   clearStore()
 })
