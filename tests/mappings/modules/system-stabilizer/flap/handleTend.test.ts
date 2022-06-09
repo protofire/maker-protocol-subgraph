73aa992f93cb8e23729fadf2043b7393d7406422
NMohm <n.accounts@mohm-mail.de>
2022-06-09 11:23:38 +0000

    use matchstick#describe

--- tests/mappings/modules/system-stabilizer/flap/handleTend.test.ts
+++ tests/mappings/modules/system-stabilizer/flap/handleTend.test.ts
@@ -1,43 +1,45 @@
 import { Bytes, Address, BigInt } from '@graphprotocol/graph-ts'
-import { test, clearStore, assert } from 'matchstick-as'
+import { test, clearStore, assert, describe } from 'matchstick-as'
 import { LogNote } from '../../../../../generated/Flap/Flapper'
 import { Auctions, system as systemModule } from '../../../../../src/entities'
 import { handleTend } from '../../../../../src/mappings/modules/system-stabilizer/flap'
 import { tests } from '../../../../../src/mappings/modules/tests'
 import { mockDebt } from '../../../../helpers/mockedFunctions'
 
-test('Flapper#handleTend changes the highestBidder and amount', () => {
-  let signature = '0x4712f803'
+describe('Flapper#handleTend', () => {
+  test('updates the highestBidder, bidAmount and the endTime', () => {
+    let signature = '0x4712f803'
 
-  let id = '50'
-  let bid = '100000000000000000000'
-  let defaultTTL = BigInt.fromString('500')
+    let id = '50'
+    let bid = '100000000000000000000'
+    let defaultTTL = BigInt.fromString('500')
 
-  let event = changetype<LogNote>(
-    tests.helpers.events.getNewEvent([
-      tests.helpers.params.getBytes('sig', Bytes.fromHexString(signature)),
-      tests.helpers.params.getBytes('usr', Bytes.fromUTF8('')),
-      tests.helpers.params.getBytes('arg1', changetype<Bytes>(Bytes.fromBigInt(BigInt.fromString(id)))),
-      tests.helpers.params.getBytes('arg2', changetype<Bytes>(Bytes.fromBigInt(BigInt.fromString(bid)))),
-    ]),
-  )
-  event.block.timestamp = BigInt.fromString('250')
+    let event = changetype<LogNote>(
+      tests.helpers.events.getNewEvent([
+        tests.helpers.params.getBytes('sig', Bytes.fromHexString(signature)),
+        tests.helpers.params.getBytes('usr', Bytes.fromUTF8('')),
+        tests.helpers.params.getBytes('arg1', changetype<Bytes>(Bytes.fromBigInt(BigInt.fromString(id)))),
+        tests.helpers.params.getBytes('arg2', changetype<Bytes>(Bytes.fromBigInt(BigInt.fromString(bid)))),
+      ]),
+    )
+    event.block.timestamp = BigInt.fromString('250')
 
-  let auction = Auctions.loadOrCreateAuction(id + '-0', event)
-  auction.highestBidder = Address.fromString('0x4d95a049d5b0b7d32058cd3f2163015747522e99')
-  auction.bidAmount = BigInt.fromString(bid)
-  auction.save()
+    let auction = Auctions.loadOrCreateAuction(id + '-0', event)
+    auction.highestBidder = Address.fromString('0x4d95a049d5b0b7d32058cd3f2163015747522e99')
+    auction.bidAmount = BigInt.fromString(bid)
+    auction.save()
 
-  mockDebt()
-  let system = systemModule.getSystemState(event)
-  system.surplusAuctionBidDuration = defaultTTL
-  system.save()
+    mockDebt()
+    let system = systemModule.getSystemState(event)
+    system.surplusAuctionBidDuration = defaultTTL
+    system.save()
 
-  handleTend(event)
+    handleTend(event)
 
-  assert.fieldEquals('Auction', auction.id, 'highestBidder', event.transaction.from.toHexString())
-  assert.fieldEquals('Auction', auction.id, 'endTime', event.block.timestamp.plus(defaultTTL).toString())
-  assert.fieldEquals('Auction', auction.id, 'bidAmount', bid)
+    assert.fieldEquals('Auction', auction.id, 'highestBidder', event.transaction.from.toHexString())
+    assert.fieldEquals('Auction', auction.id, 'endTime', event.block.timestamp.plus(defaultTTL).toString())
+    assert.fieldEquals('Auction', auction.id, 'bidAmount', bid)
 
-  clearStore()
+    clearStore()
+  })
 })
