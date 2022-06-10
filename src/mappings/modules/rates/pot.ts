1512fba7031df15fe0a830d22c1e7cdca9759261
Tomas Rawski <rtomas@gmail.com>
2022-06-10 11:14:10 -0300

    wip

--- src/mappings/modules/rates/pot.ts
+++ src/mappings/modules/rates/pot.ts
@@ -7,10 +7,16 @@ import { system as systemModule } from '../../../entities'
 export function handleFile(event: LogNote): void {
   let what = event.params.arg1.toString()
   let data = bytes.toUnsignedInt(event.params.arg2)
+  let signature = event.params.sig.toHexString()
 
-  if (what == 'dsr') {
-    let system = systemModule.getSystemState(event)
-    system.savingsRate = units.fromRay(data)
-    system.save()
+  if (signature == '0x29ae8114') {
+    if (what == 'dsr') {
+      let system = systemModule.getSystemState(event)
+      system.savingsRate = units.fromRay(data)
+      system.save()
+    }
+  } else if (signature == '0xd4e8be83') {
+    if (what == 'dsr') {
+    }
   }
 }
