702f0ad758bb128554f7965323aa06f3ea42306c
Tomas Rawski <rtomas@gmail.com>
2022-06-10 11:24:46 -0300

    wip

--- src/mappings/modules/rates/pot.ts
+++ src/mappings/modules/rates/pot.ts
@@ -2,21 +2,26 @@ import { bytes, units } from '@protofire/subgraph-toolkit'
 
 import { LogNote } from '../../../../generated/Pot/Pot'
 
-import { system as systemModule } from '../../../entities'
+import { system, system as systemModule } from '../../../entities'
 
 export function handleFile(event: LogNote): void {
   let what = event.params.arg1.toString()
-  let data = bytes.toUnsignedInt(event.params.arg2)
+
   let signature = event.params.sig.toHexString()
 
   if (signature == '0x29ae8114') {
     if (what == 'dsr') {
       let system = systemModule.getSystemState(event)
+      let data = bytes.toUnsignedInt(event.params.arg2)
+
       system.savingsRate = units.fromRay(data)
       system.save()
     }
   } else if (signature == '0xd4e8be83') {
     if (what == 'dsr') {
+      let system = systemModule.getSystemState(event)
+      let data = bytes.toAddress(event.params.arg2)
+      system.potVowContract = data
     }
   }
 }
