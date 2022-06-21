d32d9532122f9296566e47e0cf5a7c37c07e32e3
NMohm <63843013+NMohm@users.noreply.github.com>
2022-06-21 15:41:00 +0200

    Function Vow#handleFile (#66)

--- tests/mappings/modules/system-stabilizer/vow/handleFile.test.ts
+++ tests/mappings/modules/system-stabilizer/vow/handleFile.test.ts
@@ -1,51 +1,89 @@
-import { Bytes, Address, BigInt, BigDecimal } from "@graphprotocol/graph-ts";
-import { units } from "@protofire/subgraph-toolkit";
-import { test, clearStore, assert } from "matchstick-as";
-import { LogNote } from "../../../../../generated/Vow/Vow";
-import { handleCage, handleFile } from "../../../../../src/mappings/modules/system-stabilizer/vow";
-import { tests } from "../../../../../src/mappings/modules/tests";
-import { mockDebt } from "../../../../helpers/mockedFunctions";
+import { Bytes, BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
+import { units } from '@protofire/subgraph-toolkit'
+import { test, clearStore, assert, describe, beforeAll, afterAll } from 'matchstick-as'
+import { LogNote } from '../../../../../generated/Vow/Vow'
+import { handleFile } from '../../../../../src/mappings/modules/system-stabilizer/vow'
+import { tests } from '../../../../../src/mappings/modules/tests'
+import { mockDebt } from '../../../../helpers/mockedFunctions'
 
-let amount = "100500000000000000000000000000000000000000000000" // 100.5 (rad) 100500000000000000000000000000 (wad)
-let sig = "0x29ae8114"
+let amount = '100500000000000000000000000000000000000000000000' // 100.5 (rad) 100500000000000000000000000000 (wad)
+let address = '0xa4f79bc4a5612bdda35904fdf55fc4cb53d1bff6'
 
 type TransformAmount = (n: BigInt) => BigDecimal
 
-function checkHandleFile(what: string, field: string, transformAmount: TransformAmount): void{
-    let event = changetype<LogNote>(tests.helpers.events.getNewEvent([
-        tests.helpers.params.getBytes("sig", Bytes.fromHexString(sig)),
-        tests.helpers.params.getBytes("usr", Bytes.fromUTF8("")),
-        tests.helpers.params.getBytes("arg1", Bytes.fromUTF8(what)),
-        tests.helpers.params.getBytes("arg2", Bytes.fromUint8Array(Bytes.fromBigInt(BigInt.fromString(amount)).reverse())),
-    ]))
-
-    handleFile(event)
-
-    assert.fieldEquals("SystemState", "current", field, transformAmount(BigInt.fromString(amount)).toString());
-    clearStore()  
+function createEvent(sig: string, what: string, data: Bytes): LogNote {
+  return changetype<LogNote>(
+    tests.helpers.events.getNewEvent([
+      tests.helpers.params.getBytes('sig', Bytes.fromHexString(sig)),
+      tests.helpers.params.getBytes('usr', Bytes.fromUTF8('')),
+      tests.helpers.params.getBytes('arg1', Bytes.fromUTF8(what)),
+      tests.helpers.params.getBytes('arg2', data),
+    ]),
+  )
 }
 
-function returnPlainAmount(amount: BigInt): BigDecimal{
-    return amount.toBigDecimal()
+function checkUintHandleFile(what: string, field: string, transformAmount: TransformAmount): void {
+  let sig = '0x29ae8114'
+  let data = Bytes.fromUint8Array(Bytes.fromBigInt(BigInt.fromString(amount)).reverse())
+  let event = createEvent(sig, what, data)
+
+  handleFile(event)
+
+  assert.fieldEquals('SystemState', 'current', field, transformAmount(BigInt.fromString(amount)).toString())
 }
 
-test("Vow#handleFile for what=wait.", () => {
+function checkAddressHandleFile(what: string, field: string, data: Address): void {
+  let sig = '0xd4e8be83'
+  let dataBytes = changetype<Bytes>(data)
+  let event = createEvent(sig, what, dataBytes)
+
+  handleFile(event)
+
+  assert.fieldEquals('SystemState', 'current', field, data.toHexString())
+}
+
+function returnPlainAmount(amount: BigInt): BigDecimal {
+  return amount.toBigDecimal()
+}
+
+describe('Vow#handleFile', () => {
+  beforeAll(() => {
     mockDebt()
-    checkHandleFile("wait", "debtAuctionDelay", returnPlainAmount)
-})
+  })
 
-test("Vow#handleFile for what=bump",() => {
-    checkHandleFile("bump", "surplusAuctionLotSize", units.fromRad)
-})
+  describe('For file(bytes32 what, uint256 data) with what=(wait,bump,sump,dump,hump)', () => {
+    test('For what=wait.', () => {
+      checkUintHandleFile('wait', 'debtAuctionDelay', returnPlainAmount)
+    })
 
-test("Vow#handleFile for what=sump",() => {
-    checkHandleFile("sump", "debtAuctionBidSize", units.fromRad)
-})
+    test('For what=bump', () => {
+      checkUintHandleFile('bump', 'surplusAuctionLotSize', units.fromRad)
+    })
 
-test("Vow#handleFile for what=dump",() => {
-    checkHandleFile("dump", "debtAuctionInitialLotSize", units.fromWad)
-})
+    test('For what=sump', () => {
+      checkUintHandleFile('sump', 'debtAuctionBidSize', units.fromRad)
+    })
 
-test("Vow#handleFile for what=hump",() => {
-    checkHandleFile("hump", "surplusAuctionBuffer", units.fromRad)
+    test('For what=dump', () => {
+      checkUintHandleFile('dump', 'debtAuctionInitialLotSize', units.fromWad)
+    })
+
+    test('For what=hump', () => {
+      checkUintHandleFile('hump', 'surplusAuctionBuffer', units.fromRad)
+    })
+  })
+
+  describe('For file(bytes32 what, address data) with what=(flapper,flopper)', () => {
+    test('For what=Flapper', () => {
+      checkAddressHandleFile('flapper', 'vowFlapperContract', Address.fromString(address))
+    })
+
+    test('For what=Flopper', () => {
+      checkAddressHandleFile('flopper', 'vowFlopperContract', Address.fromString(address))
+    })
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
 })
