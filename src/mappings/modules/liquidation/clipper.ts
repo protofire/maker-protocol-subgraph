da6451a73ee7ee45f147aaf39fe967e4e169f289
Tomas Rawski <rtomas@gmail.com>
2022-06-28 14:52:07 -0300

    Clipper#handleRedo building the mapping for Redo

--- src/mappings/modules/liquidation/clipper.ts
+++ src/mappings/modules/liquidation/clipper.ts
@@ -1,5 +1,6 @@
+import { BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { units } from '@protofire/subgraph-toolkit'
-import { Kick } from '../../../../generated/Clipper/Clipper'
+import { Kick, Redo, Clipper } from '../../../../generated/Clipper/Clipper'
 import { SaleAuctions } from '../../../entities'
 
 export function handleKick(event: Kick): void {
@@ -21,4 +22,27 @@ export function handleKick(event: Kick): void {
   saleAuction.save()
 
   // FYI not saving the COIN value.
+}
+
+export function handleRedo(event: Redo): void {
+  let idStr = event.params.id.toString()
+  let tab = units.fromRad(event.params.tab)
+  let lot = units.fromWad(event.params.lot)
+  let usr = event.params.usr.toHexString()
+  let kpr = event.params.kpr.toHexString()
+  let top = units.fromRay(event.params.top)
+
+  let saleAuction = SaleAuctions.loadOrCreateSaleAuction(idStr, event)
+  saleAuction.resetedAt = event.block.timestamp
+
+  /* uint256 feedPrice = getFeedPrice();
+  top = rmul(feedPrice, buf); */
+  let clipperContract = Clipper.bind(Address.fromString('0xc67963a226eddd77B91aD8c421630A1b0AdFF270'))
+
+  clipperContract.
+
+  let newTop = BigDecimal.fromString('0')
+  saleAuction.startingPrice = newTop
+
+  saleAuction.save()
 }
