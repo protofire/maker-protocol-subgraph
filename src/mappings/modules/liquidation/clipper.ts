4484bff885c60c9179a10d894ba384a5d067c7d6
Tomas Rawski <rtomas@gmail.com>
2022-06-28 15:31:47 -0300

    mapping and testing for event Redo from Clipper

--- src/mappings/modules/liquidation/clipper.ts
+++ src/mappings/modules/liquidation/clipper.ts
@@ -26,23 +26,11 @@ export function handleKick(event: Kick): void {
 
 export function handleRedo(event: Redo): void {
   let idStr = event.params.id.toString()
-  let tab = units.fromRad(event.params.tab)
-  let lot = units.fromWad(event.params.lot)
-  let usr = event.params.usr.toHexString()
-  let kpr = event.params.kpr.toHexString()
   let top = units.fromRay(event.params.top)
 
   let saleAuction = SaleAuctions.loadOrCreateSaleAuction(idStr, event)
   saleAuction.resetedAt = event.block.timestamp
-
-  /* uint256 feedPrice = getFeedPrice();
-  top = rmul(feedPrice, buf); */
-  let clipperContract = Clipper.bind(Address.fromString('0xc67963a226eddd77B91aD8c421630A1b0AdFF270'))
-
-  clipperContract.
-
-  let newTop = BigDecimal.fromString('0')
-  saleAuction.startingPrice = newTop
+  saleAuction.startingPrice = top
 
   saleAuction.save()
 }
