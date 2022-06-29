3a83d2937f1a5d34a6a67c040e73d4ed00999b7f
Tomas Rawski <rtomas@gmail.com>
2022-06-29 16:47:41 -0300

    mappping and testing for event Take in the contract Clipper

--- src/mappings/modules/liquidation/clipper.ts
+++ src/mappings/modules/liquidation/clipper.ts
@@ -1,8 +1,9 @@
 import { units } from '@protofire/subgraph-toolkit'
-import { Kick } from '../../../../generated/Clipper/Clipper'
+import { BigDecimal } from '@graphprotocol/graph-ts'
+import { Kick as KickEvent, Take as TakeEvent } from '../../../../generated/Clipper/Clipper'
 import { SaleAuctions } from '../../../entities'
 
-export function handleKick(event: Kick): void {
+export function handleKick(event: KickEvent): void {
   let idStr = event.params.id.toString()
   let tab = units.fromRad(event.params.tab)
   let lot = units.fromWad(event.params.lot)
@@ -21,4 +22,24 @@ export function handleKick(event: Kick): void {
   saleAuction.save()
 
   // FYI not saving the COIN value.
+}
+
+export function handleTake(event: TakeEvent): void {
+  let idStr = event.params.id.toString()
+  let tab = units.fromRad(event.params.tab)
+  let lot = units.fromWad(event.params.lot)
+
+  let saleAuction = SaleAuctions.loadOrCreateSaleAuction(idStr, event)
+
+  if (lot == BigDecimal.fromString('0')) {
+    saleAuction.isActive = false
+  } else if (tab == BigDecimal.fromString('0')) {
+    saleAuction.isActive = false
+  } else {
+    saleAuction.amountDaiToRaise = tab
+    saleAuction.amountCollateralToSell = lot
+    saleAuction.boughtAt = event.block.timestamp
+  }
+
+  saleAuction.save()
 }
---
+++ tests/mappings/modules/liquidation/clipper/handleTake.test.ts
@@ -0,0 +1,117 @@
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { test, assert, clearStore, describe, afterAll } from 'matchstick-as'
+import { tests } from '../../../../../src/mappings/modules/tests'
+import { Take as TakeEvent } from '../../../../../generated/Clipper/Clipper'
+import { handleTake } from '../../../../../src/mappings/modules/liquidation/clipper'
+import { SaleAuctions } from '../../../../../src/entities'
+
+function createSaleAuction(id: BigInt, event: TakeEvent): void {
+  let idStr = id.toString()
+
+  event.block.timestamp = BigInt.fromI32(1)
+  let saleAuction = SaleAuctions.loadOrCreateSaleAuction(idStr, event)
+  saleAuction.isActive = true
+  saleAuction.save()
+}
+
+describe('Clipper#handleTake', () => {
+  describe('When [lot] is 0', () => {
+    test('Set isActite to false from SaleAuction', () => {
+      let id = BigInt.fromString('2')
+      let max = BigInt.fromString('10000000000000000000000000000') // 10 ray
+      let price = BigInt.fromString('11000000000000000000000000000') // 11 ray
+      let owe = BigInt.fromString('1000000000000000000000000000') // 1 ray
+      let tab = BigInt.fromString('0') // 0 rad
+      let lot = BigInt.fromString('101000000000000000000') // 101 wad
+      let usr = Address.fromString('0x0000000000000000000000000000000000001111')
+      let event = changetype<TakeEvent>(
+        tests.helpers.events.getNewEvent([
+          tests.helpers.params.getBigInt('id', id),
+          tests.helpers.params.getBigInt('max', max),
+          tests.helpers.params.getBigInt('price', price),
+          tests.helpers.params.getBigInt('owe', owe),
+          tests.helpers.params.getBigInt('tab', tab),
+          tests.helpers.params.getBigInt('lot', lot),
+          tests.helpers.params.getAddress('usr', usr),
+        ]),
+      )
+      createSaleAuction(id, event)
+
+      event.block.timestamp = BigInt.fromI32(1001)
+
+      handleTake(event)
+
+      assert.fieldEquals('SaleAuction', id.toString(), 'isActive', 'false')
+      assert.fieldEquals('SaleAuction', id.toString(), 'updatedAt', '1001')
+    })
+  })
+
+  describe('When [tab] is 0', () => {
+    test('Set isActite to false from SaleAuction', () => {
+      let id = BigInt.fromString('2')
+      let max = BigInt.fromString('10000000000000000000000000000') // 10 ray
+      let price = BigInt.fromString('11000000000000000000000000000') // 11 ray
+      let owe = BigInt.fromString('1000000000000000000000000000') // 1 ray
+      let tab = BigInt.fromString('0') // 0 rad
+      let lot = BigInt.fromString('101000000000000000000') // 101 wad
+      let usr = Address.fromString('0x0000000000000000000000000000000000001111')
+      let event = changetype<TakeEvent>(
+        tests.helpers.events.getNewEvent([
+          tests.helpers.params.getBigInt('id', id),
+          tests.helpers.params.getBigInt('max', max),
+          tests.helpers.params.getBigInt('price', price),
+          tests.helpers.params.getBigInt('owe', owe),
+          tests.helpers.params.getBigInt('tab', tab),
+          tests.helpers.params.getBigInt('lot', lot),
+          tests.helpers.params.getAddress('usr', usr),
+        ]),
+      )
+      createSaleAuction(id, event)
+
+      event.block.timestamp = BigInt.fromString('1001')
+
+      handleTake(event) // aca falla
+
+      assert.fieldEquals('SaleAuction', id.toString(), 'isActive', 'false')
+      assert.fieldEquals('SaleAuction', id.toString(), 'updatedAt', '1001')
+    })
+  })
+
+  describe('When [tab] and [lot] ar not 0', () => {
+    test('Updates from SaleAuction the fields amountDaiToRaise, amountCollateralToSell, boughtAt & updatedAt', () => {
+      let id = BigInt.fromString('2')
+      let max = BigInt.fromString('10000000000000000000000000000') // 10 ray
+      let price = BigInt.fromString('11000000000000000000000000000') // 11 ray
+      let owe = BigInt.fromString('1000000000000000000000000000') // 1 ray
+      let tab = BigInt.fromString('5000000000000000000000000000000000000000000000') // 5 rad
+      let lot = BigInt.fromString('101000000000000000000') // 101 wad
+      let usr = Address.fromString('0x0000000000000000000000000000000000001111')
+      let event = changetype<TakeEvent>(
+        tests.helpers.events.getNewEvent([
+          tests.helpers.params.getBigInt('id', id),
+          tests.helpers.params.getBigInt('max', max),
+          tests.helpers.params.getBigInt('price', price),
+          tests.helpers.params.getBigInt('owe', owe),
+          tests.helpers.params.getBigInt('tab', tab),
+          tests.helpers.params.getBigInt('lot', lot),
+          tests.helpers.params.getAddress('usr', usr),
+        ]),
+      )
+
+      createSaleAuction(id, event)
+
+      event.block.timestamp = BigInt.fromI32(1001)
+
+      handleTake(event)
+
+      assert.fieldEquals('SaleAuction', id.toString(), 'amountDaiToRaise', '5')
+      assert.fieldEquals('SaleAuction', id.toString(), 'amountCollateralToSell', '101')
+      assert.fieldEquals('SaleAuction', id.toString(), 'updatedAt', '1001')
+      assert.fieldEquals('SaleAuction', id.toString(), 'boughtAt', '1001')
+    })
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+})
