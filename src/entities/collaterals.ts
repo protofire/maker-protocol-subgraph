d106c885d481ca51fc67039feab2dab1049c08a8
Luis Lopez <luis.lopez@altoros.com>
2022-07-01 16:40:07 -0300

    fix imports

--- src/entities/collaterals.ts
+++ src/entities/collaterals.ts
@@ -1,26 +1,25 @@
-import { BigDecimal, ethereum } from "@graphprotocol/graph-ts"
-import { decimal } from "@protofire/subgraph-toolkit"
-import { Collateral } from "../../generated/schema"
+import { ethereum } from '@graphprotocol/graph-ts'
+import { decimal } from '@protofire/subgraph-toolkit'
+import { Collateral } from '../../generated/schema'
 export namespace collaterals {
-	function getGemId(ilk: string, user: string): string {
-		return `${user}-${ilk}`
-	}
+  function getGemId(ilk: string, user: string): string {
+    return `${user}-${ilk}`
+  }
 
-	export function loadOrCreateCollateral(event: ethereum.Event, ilk: string, owner: string): Collateral {
-		let id = getGemId(ilk, owner)
-		let entity = Collateral.load(id)
-		if (entity == null) {
-			entity = new Collateral(id)
-			entity.type = ilk
-			entity.amount = decimal.ZERO
-			entity.createdAt = event.block.timestamp
-			entity.createdAtBlock = event.block.number
-			entity.createdAtTransaction = event.transaction.hash
-			entity.modifiedAt = event.block.timestamp
-			entity.modifiedAtBlock = event.block.number
-			entity.modifiedAtTransaction = event.transaction.hash
-		}
-		return entity as Collateral
-	}
-
+  export function loadOrCreateCollateral(event: ethereum.Event, ilk: string, owner: string): Collateral {
+    let id = getGemId(ilk, owner)
+    let entity = Collateral.load(id)
+    if (entity == null) {
+      entity = new Collateral(id)
+      entity.type = ilk
+      entity.amount = decimal.ZERO
+      entity.createdAt = event.block.timestamp
+      entity.createdAtBlock = event.block.number
+      entity.createdAtTransaction = event.transaction.hash
+      entity.modifiedAt = event.block.timestamp
+      entity.modifiedAtBlock = event.block.number
+      entity.modifiedAtTransaction = event.transaction.hash
+    }
+    return entity as Collateral
+  }
 }
